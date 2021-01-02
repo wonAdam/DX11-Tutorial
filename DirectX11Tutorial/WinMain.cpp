6ed842aab763fbac529a37c02570ad4609c7a60a
wonAdam <ehdgus5500@gmail.com>
2021-01-02 16:15:30 +0900

    main setup

---
+++ DirectX11Tutorial/WinMain.cpp
@@ -0,0 +1,14 @@
+#include <Windows.h>
+
+
+int CALLBACK WinMain(
+	_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPSTR     lpCmdLine,
+	_In_ int       nCmdShow
+)
+{
+
+	while (true);
+	return 0;
+}
