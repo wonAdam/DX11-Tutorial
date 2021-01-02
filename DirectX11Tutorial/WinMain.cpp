fe9379c9a49bcb6d1c45b4cd4bacd9ad7b051aa4
wonAdam <ehdgus5500@gmail.com>
2021-01-02 19:07:04 +0900

    window creation success

--- DirectX11Tutorial/WinMain.cpp
+++ DirectX11Tutorial/WinMain.cpp
@@ -1,13 +1,71 @@
 #include <Windows.h>
 
 
-int CALLBACK WinMain(
-	_In_ HINSTANCE hInstance,
-	_In_opt_ HINSTANCE hPrevInstance,
-	_In_ LPSTR     lpCmdLine,
-	_In_ int       nCmdShow
+LRESULT CALLBACK WndProc(
+	HWND   hWnd,
+	UINT   message,
+	WPARAM wParam,
+	LPARAM lParam
 )
 {
+	
+
+	return DefWindowProc(hWnd, message, wParam, lParam);
+}
+
+int CALLBACK WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR     lpCmdLine,
+	int       nCmdShow
+)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	const auto pClassName = L"hw3dbuttslol";
+	// register window class
+	WNDCLASSEX wc;
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = pClassName;
+	wc.hIcon = NULL;
+	wc.hIconSm = NULL;
+
+	RegisterClassEx(&wc);
+
+	// Calculate main window size
+	RECT rc = { 0, 0, 640, 480 };
+	// create window instance
+	HWND hWnd = CreateWindowEx(
+		0, pClassName, L"Happy Hard Window",
+		WS_CAPTION | WS_MINIMIZE | WS_SYSMENU,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		rc.right - rc.left, rc.bottom - rc.top,
+		NULL, NULL, hInstance, NULL);
+
+	if (hWnd == NULL)
+	{
+		return 1;
+	}
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	// message pump
+	MSG msg;
+	while (GetMessage(&msg, nullptr, 0, 0) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
 
 	while (true);
 	return 0;
