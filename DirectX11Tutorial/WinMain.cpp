4b1834815f08129b23e3b7366c4ca7da3478c014
wonAdam <ehdgus5500@gmail.com>
2021-01-03 12:53:40 +0900

    make codes simillar to chili

--- DirectX11Tutorial/WinMain.cpp
+++ DirectX11Tutorial/WinMain.cpp
@@ -1,17 +1,11 @@
 #include <Windows.h>
 
-
 LRESULT CALLBACK WndProc(
 	HWND   hWnd,
 	UINT   message,
 	WPARAM wParam,
 	LPARAM lParam
-)
-{
-	
-
-	return DefWindowProc(hWnd, message, wParam, lParam);
-}
+);
 
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
@@ -20,35 +14,33 @@ int CALLBACK WinMain(
 	int       nCmdShow
 )
 {
-	UNREFERENCED_PARAMETER(hPrevInstance);
-	UNREFERENCED_PARAMETER(lpCmdLine);
-
 	const auto pClassName = L"hw3dbuttslol";
 	// register window class
-	WNDCLASSEX wc;
+	WNDCLASSEX wc = { 0 };
 	wc.cbSize = sizeof(WNDCLASSEX);
-	wc.style = CS_HREDRAW | CS_VREDRAW;
-	wc.lpfnWndProc = WndProc;
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = DefWindowProc;
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = hInstance;
-	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-	wc.lpszMenuName = NULL;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
 	wc.lpszClassName = pClassName;
-	wc.hIcon = NULL;
-	wc.hIconSm = NULL;
+	wc.hIcon = nullptr;
+	wc.hIconSm = nullptr;
 
 	RegisterClassEx(&wc);
 
-	// Calculate main window size
+	// Main window size
 	RECT rc = { 0, 0, 640, 480 };
 	// create window instance
 	HWND hWnd = CreateWindowEx(
 		0, pClassName, L"Happy Hard Window",
-		WS_CAPTION | WS_MINIMIZE | WS_SYSMENU,
-		CW_USEDEFAULT, CW_USEDEFAULT,
-		rc.right - rc.left, rc.bottom - rc.top,
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		200, 200,
+		//rc.right - rc.left, rc.bottom - rc.top,
+		640, 480,
 		NULL, NULL, hInstance, NULL);
 
 	if (hWnd == NULL)
@@ -56,17 +48,28 @@ int CALLBACK WinMain(
 		return 1;
 	}
 
-	ShowWindow(hWnd, nCmdShow);
+	ShowWindow(hWnd, SW_SHOW);
 	UpdateWindow(hWnd);
 
-	// message pump
-	MSG msg;
-	while (GetMessage(&msg, nullptr, 0, 0) > 0)
-	{
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	}
+	//// message pump
+	//MSG msg;
+	//while (GetMessage(&msg, nullptr, 0, 0) > 0)
+	//{
+	//	TranslateMessage(&msg);
+	//	DispatchMessage(&msg);
+	//}
 
 	while (true);
 	return 0;
+}
+
+
+LRESULT CALLBACK WndProc(
+	HWND   hWnd,
+	UINT   message,
+	WPARAM wParam,
+	LPARAM lParam
+)
+{
+	return DefWindowProc(hWnd, message, wParam, lParam);
 }
