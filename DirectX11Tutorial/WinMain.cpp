e0ba2f401cdf27dcbe339b45f57b275600d3eea1
wonAdam <ehdgus5500@gmail.com>
2021-01-03 16:22:13 +0900

    Window Framework : Window Class

--- DirectX11Tutorial/WinMain.cpp
+++ DirectX11Tutorial/WinMain.cpp
@@ -1,13 +1,4 @@
-#include <Windows.h>
-#include <sstream>
-#include "WindowsMessageMap.h"
-
-LRESULT CALLBACK WndProc(
-	HWND   hWnd,
-	UINT   message,
-	WPARAM wParam,
-	LPARAM lParam
-);
+#include "Window.h"
 
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
@@ -16,44 +7,8 @@ int CALLBACK WinMain(
 	int       nCmdShow
 )
 {
-	LPCSTR pClassName = "hw3dbuttslol";
-	// register window class
-	WNDCLASSEX wc = { 0 };
-	wc.cbSize = sizeof(WNDCLASSEX);
-	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = WndProc;
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
-	wc.hInstance = hInstance;
-	wc.hCursor = nullptr;
-	wc.hbrBackground = nullptr;
-	wc.lpszMenuName = nullptr;
-	wc.lpszClassName = pClassName;
-	wc.hIcon = nullptr;
-	wc.hIconSm = nullptr;
+	Window wnd(800, 300, "Donkey Fart Box");
 
-	RegisterClassEx(&wc);
-
-	// Main window size
-	RECT rc = { 0, 0, 640, 480 };
-	// create window instance
-	HWND hWnd = CreateWindowEx(
-		0, pClassName, "Happy Hard Window",
-		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
-		200, 200,
-		//rc.right - rc.left, rc.bottom - rc.top,
-		640, 480,
-		NULL, NULL, hInstance, NULL);
-
-	if (hWnd == NULL)
-	{
-		return 1;
-	}
-
-	ShowWindow(hWnd, SW_SHOW);
-	UpdateWindow(hWnd);
-
-	// message loop
 	MSG msg;
 	BOOL gResult;
 	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
@@ -62,63 +17,12 @@ int CALLBACK WinMain(
 		DispatchMessage(&msg);
 	}
 
-	// GetMessage Error
 	if (gResult == -1)
 	{
 		return -1;
 	}
-	else
-	{
-		return msg.wParam;
-	}
+
+	return msg.wParam;
 }
 
 
-LRESULT CALLBACK WndProc(
-	HWND   hWnd,
-	UINT   message,
-	WPARAM wParam,
-	LPARAM lParam
-)
-{
-	static WindowsMessageMap mm;
-	OutputDebugString( mm(message, lParam, wParam).c_str() );
-
-	switch (message)
-	{
-		case WM_CLOSE:
-			PostQuitMessage(69);
-			break;
-		case WM_KEYDOWN:
-			if (wParam == 'F')
-			{
-				SetWindowText(hWnd, "Respect");
-			}
-			break;
-		case WM_KEYUP: 
-			if (wParam == 'F')
-			{
-				SetWindowText(hWnd, "Danger");
-			}
-			break;
-		case WM_CHAR: // Translated by TranslateMessageuse, this for character input, letter
-			{
-				static std::string title;
-				title.push_back((char)wParam);
-				SetWindowText(hWnd, title.c_str());
-			}
-			break;
-		case WM_LBUTTONDOWN:
-			{
-			const POINTS pt = MAKEPOINTS(lParam);
-			std::ostringstream oss;
-			oss << "(" << pt.x << ", " << pt.y << ")";
-			SetWindowText(hWnd, oss.str().c_str());
-			}
-			
-			break;
-	}
-
-	// behavior
-	return DefWindowProc(hWnd, message, wParam, lParam);
-}
